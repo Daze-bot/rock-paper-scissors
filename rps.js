c4c98bac4b3c0025b011a1403588c530224f3c2d
Code4Daze <jevon.fillette@gmail.com>
2022-05-12 15:25:33 -0400

    Create the function for one round of play
    This function compares the player selection to the computer selection
    and returns a string with the outcome of the game.

--- rps.js
+++ rps.js
@@ -1,17 +1,42 @@
-function computerPlay () {
+function computerPlay() {
   let randomNum = Math.floor(Math.random()*3);
   
   if (randomNum === 0) {
-    return "Rock";
+    return "ROCK";
   } else if (randomNum === 1) {
-    return "Paper";
+    return "PAPER";
   } else {
-    return "Scissors";
+    return "SCISSORS";
   }
 }
+let computerSelection = computerPlay();
+console.log(computerSelection);
 
 let playerSelectionInput = prompt("Please choose Rock, Paper, or Scissors and type it below" , "");
 let playerSelection = playerSelectionInput.toUpperCase();
 // Converted to uppercase to allow users to have case not matter on user input.
 // When using playerSelection moving forward, will have to reference uppercase.
+console.log(playerSelection);
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+    return "You both chose Rock.  It's a tie!";
+  } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    return "Paper beats Rock.  You lose!";
+  } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    return "Rock beats Scissors.  You win!";
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    return "Paper beats Rock.  You win!";
+  } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+    return "You both chose Paper.  It's a tie!";
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    return "Scissors beats Paper. You lose!";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    return "Rock beats Scissors.  You lose!";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    return "Scissors beats Paper. You win!";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
+    return "You both chose Scissors.  It's a tie!";
+  }
+}
+console.log(playRound(playerSelection, computerSelection));
