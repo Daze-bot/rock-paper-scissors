fa2fcd397b55aba348378a896c4de49aa4ef1956
Code4Daze <jevon.fillette@gmail.com>
2022-05-12 17:27:34 -0400

    Edit the game to be 5 rounds and have a winner
    Loop the playRound function 5 times in order to simulate 5 rounds of
    play.  Create a way to keep track of the score after each round using a
    global variable that is edited each round.  Announce the final winner.

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let randomNum = Math.floor(Math.random()*3);
   
@@ -9,34 +12,58 @@ function computerPlay() {
     return "SCISSORS";
   }
 }
-let computerSelection = computerPlay();
-console.log(computerSelection);
-
-let playerSelectionInput = prompt("Please choose Rock, Paper, or Scissors and type it below" , "");
-let playerSelection = playerSelectionInput.toUpperCase();
-// Converted to uppercase to allow users to have case not matter on user input.
-// When using playerSelection moving forward, will have to reference uppercase.
-console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "ROCK" && computerSelection === "ROCK") {
     return "You both chose Rock.  It's a tie!";
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    computerScore += 1;
     return "Paper beats Rock.  You lose!";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    playerScore += 1;
     return "Rock beats Scissors.  You win!";
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    playerScore += 1;
     return "Paper beats Rock.  You win!";
   } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
     return "You both chose Paper.  It's a tie!";
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    computerScore += 1;
     return "Scissors beats Paper. You lose!";
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    computerScore += 1;
     return "Rock beats Scissors.  You lose!";
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    playerScore += 1;
     return "Scissors beats Paper. You win!";
   } else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
     return "You both chose Scissors.  It's a tie!";
+  } else {
+    computerScore += 1;
+    return "You didn't choose a valid answer. You lose!";
   }
 }
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (let i = 1; i < 6; i++) {
+    let playerSelectionInput = prompt("Round " + i + "\n\nPlease choose Rock, Paper, or Scissors and type it below:" , "");
+    let playerSelection = playerSelectionInput.toUpperCase();
+    let computerSelection = computerPlay();
+
+    playRound(playerSelection, computerSelection);
+
+    alert("You chose " + playerSelection + ". The computer chose " + computerSelection);
+    alert(playRound(playerSelection, computerSelection));
+    alert("The score is " + (playerScore / 2) + " to " + (computerScore / 2));
+  }
+}
+
+game();
+
+if (playerScore > computerScore) {
+  alert("After 5 rounds of play...You beat the computer with a final score of " + (playerScore / 2) + " to " + (computerScore / 2) + "!");
+} else if (playerScore < computerScore) {
+  alert("After 5 rounds of play...You lost to the computer with a final score of " + (playerScore / 2) + " to " + (computerScore / 2) + "!");
+} else {
+  alert("After 5 rounds of play...You tied the computer with a final score of " + (playerScore / 2) + " to " + (computerScore / 2) + "!");
+}
