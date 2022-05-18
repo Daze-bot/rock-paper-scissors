b73926d81600392dae689ebb4e7cad5e051fc490
Code4Daze <jevon.fillette@gmail.com>
2022-05-18 19:45:11 -0400

    Add 'New Game' button
    Page now starts with no options available.  Once 'New Game' is pressed,
    the 3 options will show along with a para to make your selection.
    
    Game also now resets on 'New Game' press

--- rps.js
+++ rps.js
@@ -3,25 +3,73 @@ let computerScore = 0;
 let roundCount = 0;
 
 const results = document.querySelector('.results');
-let buttons = document.querySelectorAll('.button');
+let gameStartBtn = document.querySelector('#gameStart');
+let choiceText = document.querySelector('.chooseButton');
+let buttonsContainer = document.querySelector('.buttonsContainer');
+
+gameStartBtn.addEventListener('click', playGame);
+
+function addButtons() {
+  while (buttonsContainer.hasChildNodes()) {
+    buttonsContainer.removeChild(buttonsContainer.lastChild);
+  }
+
+  let rockBtn = document.createElement('button');
+  let paperBtn = document.createElement('button');
+  let scissorsBtn = document.createElement('button');
+
+  rockBtn.setAttribute('id', "rock");
+  rockBtn.classList.add("button");
+  rockBtn.textContent = "Rock";
+  paperBtn.setAttribute('id', "rock");
+  paperBtn.classList.add("button");
+  paperBtn.textContent = "Paper";
+  scissorsBtn.setAttribute('id', "rock");
+  scissorsBtn.classList.add("button");
+  scissorsBtn.textContent = "Scissors";
+
+  buttonsContainer.appendChild(rockBtn);
+  buttonsContainer.appendChild(paperBtn);
+  buttonsContainer.appendChild(scissorsBtn);
+}
+
+function addStartText() {
+  choiceText.textContent = `Game Start. Make your pick below!`
+}
+
+function playGame() {
+  addStartText();
+  addButtons();
+  buttonSelection();
+}
 
 function buttonSelection() {
+  let buttons = document.querySelectorAll('.button');
   buttons.forEach((press) => {
     press.addEventListener('click', () => {
       let playerSelectionInput = press.textContent;
-      let computerSelection = computerPlay();
       let playerSelection = playerSelectionInput.toUpperCase();
+      let computerSelection = computerPlay();
       alert("You chose " + playerSelection + "\nThe computer chose " +
           computerSelection + "\n\n" + playRound(playerSelection, computerSelection));
       if (roundCount === 5 && playerScore > computerScore) {
         alert("After 5 rounds of play...You beat the computer with a final score of:\n" +
             playerScore + " to " + computerScore);
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0;    
       } else if (roundCount === 5 && playerScore < computerScore) {
         alert("After 5 rounds of play...You lost to the computer with a final score of:\n" +
             playerScore + " to " + computerScore);
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0; 
       } else if (roundCount === 5 && playerScore === computerScore) {
         alert("After 5 rounds of play...You tied the computer with a final score of:\n" +
             playerScore + " to " + computerScore);  
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0; 
       } else {
         alert("The score is " + playerScore + " to " + computerScore);
       }
@@ -82,5 +130,3 @@ function playRound(playerSelection, computerSelection) {
         "\n(Double check your spelling!)";
   }
 }
-
-buttonSelection();
